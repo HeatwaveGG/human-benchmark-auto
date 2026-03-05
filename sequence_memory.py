f3d55c4ae4ba51227e470f60725eb3fdcbc73405
Heatwave0705 <17793dxbp@gmail.com>
2026-03-05 11:46:48 +0400

    removed logic comment

--- sequence_memory.py
+++ sequence_memory.py
@@ -2,22 +2,6 @@ import pyautogui
 import keyboard
 import time
 
-"""
-Logic:
-
-make a list to simulate the squares   -
-associate 1 pair of coords to 1 square   -
-have the starting colour of the squares -
-
-loop:
-check the colour of each square (each coord associated with the square)
-when a colour changes from its original colour in a pair of coords, convert the coords into a square and add it to a list 
-stop checking the colour after a set amount of time has passed
-go over the list and click the corresponding coordinates
-clear the list
-
-"""
-
 # squares in grid
 square1 = (818, 307)
 square2 = (951, 297)
