19dda4495adfedd677a50ee036e9d51dd91c6acb
Heatwave0705 <17793dxbp@gmail.com>
2026-03-11 16:39:50 +0400

    set a fixed pixel to check and click in reaction time test

--- reaction_time.py
+++ reaction_time.py
@@ -5,13 +5,12 @@ count = 1
 
 keyboard.wait("F8")
 
-pyautogui.click()
+pyautogui.click(1790, 578)
 
-x, y = pyautogui.position()
-starting_colour = pyautogui.pixel(x, y) # Red
+starting_colour = pyautogui.pixel(1790, 578) # Red
 
 while count <= 5:
-    if pyautogui.pixel(x, y) != starting_colour:
+    if pyautogui.pixel(1790, 578) != starting_colour:
         pyautogui.click()
         pyautogui.click()
         count += 1
