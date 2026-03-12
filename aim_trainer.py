53bf54744a5d78774327b1f765535053c793d841
Heatwave0705 <17793dxbp@gmail.com>
2026-03-12 23:32:32 +0400

    deleted wrong comment, changed diameter value to 39, and changed diameter to step

--- aim_trainer.py
+++ aim_trainer.py
@@ -17,20 +17,20 @@ left = 449
 right = 1471
 top = 190
 bottom = 673
-diameter = 35 # Actual circle diameter is 100, 70 was calculated by finding the side lenth of the largest square in a circle with radius 50 (diamerer 100)
+step = 39
 circle_colour = (149, 195, 232)
 white = (255, 255, 255)
 
 for i in range(30):
     found = False
     screenshot = pyautogui.screenshot()
-    for row in range((bottom - top) // diameter + 1):
+    for row in range((bottom - top) // step + 1):
         if found:
             break  
-        for col in range((right - left) // diameter + 1):
+        for col in range((right - left) // step + 1):
 
-            current_x = left + col * diameter + diameter//2
-            current_y = top + row * diameter + diameter//2
+            current_x = left + col * step + step//2
+            current_y = top + row * step + step//2
 
             color = screenshot.getpixel((current_x, current_y))
 
