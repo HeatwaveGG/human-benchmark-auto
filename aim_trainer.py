798547d0bc77af94ba449b054f5488ea13840d7e
Heatwave0705 <17793dxbp@gmail.com>
2026-05-17 18:24:15 +0400

    added colour tolerance and tweaked area values

--- aim_trainer.py
+++ aim_trainer.py
@@ -1,5 +1,6 @@
 import pyautogui
 import keyboard
+import math
 
 keyboard.wait("F8")
 pyautogui.click(950, 400)
@@ -13,13 +14,18 @@ area corners
 449 673
 """
 
-left = 449
+def colour_distance(c1, c2):
+    return math.sqrt((c1[0]-c2[0])**2 + (c1[1]-c2[1])**2 + (c1[2]-c2[2])**2)
+
+# Target circle diameter is 100
+
+left = 517
 right = 1471
-top = 190
+top = 229
 bottom = 673
-step = 39
+step = 50
 circle_colour = (149, 195, 232)
-white = (255, 255, 255)
+background = (43, 135, 209)
 
 for i in range(30):
     found = False
@@ -28,13 +34,10 @@ for i in range(30):
         if found:
             break  
         for col in range((right - left) // step + 1):
-
-            current_x = left + col * step + step//2
-            current_y = top + row * step + step//2
-
+            current_x = left + col * step + step // 2
+            current_y = top + row * step + step // 2
             color = screenshot.getpixel((current_x, current_y))
-
-            if color == circle_colour or color == white:
+            if colour_distance(color, background) > 30:
                 pyautogui.click(current_x, current_y)
                 found = True
                 break
